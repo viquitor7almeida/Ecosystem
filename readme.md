5af6eea75fe7dc0ba2aa7befd319a3c8c838e623
viquitor7almeida <viquitor2020@gmail.com>
2026-04-04 21:15:47 -0300

    feat: estrutura inicial do ecossistema de rnas

---
+++ readme.md
@@ -0,0 +1,26 @@
+## About the Project
+
+This application was created as a practical way to study core Java concepts, especially the ones that usually feel more abstract, such as:
+
+- Memory addressing  
+- Stack and heap behavior  
+- JIT (Just-In-Time Compiler)  
+- Garbage Collector  
+
+The goal is to move away from pure theory and actually observe these concepts working in a simple but dynamic environment.
+
+## What the App Does
+
+The system simulates a kind of chemical evolution between RNAs. Based on this idea, the program's behavior is built using only basic language features, such as:
+
+- Arrays to represent structures and states  
+- Logical operations to define interaction rules  
+- Loops to simulate evolution over time  
+- Input and output to track the process  
+- Object lifecycle to observe allocation, usage, and memory cleanup  
+
+## Purpose
+
+Rather than aiming for a scientifically accurate simulation, the focus of this project is to act as a learning tool. Each part of the code is designed to help visualize how Java handles memory and execution at runtime.
+
+It’s simple on the surface, but useful for understanding what’s usually hidden behind the JVM.
---
+++ rules/board.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class board {
+    
+    public static void main(String[] args) {
+        int [][] current = new int[50][50]; 
+        int [][] next = new int [50][50];
+        
+        start(current);
+        evolve(current, next);     
+    }
+
+    public static void start(int[][] current) {
+        Random generator = new Random();
+        for(int l=0; l<current.length; l++) {
+            for(int c=0; c<current[l].length; c++) {
+                double vitality = generator.nextDouble();
+                if (vitality > 0.6) {
+                    current[l][c]=1;
+                }
+                else {
+                    current[l][c]=0;
+                } 
+            System.out.print(current[l][c]==1 ? " 1:🦠 " : " 0:☠️  ");
+            }
+        System.out.println();
+        System.out.println();
+        }
+    }
+
+    public static void evolve(int[][] current, int[][] next) {
+        for(int l=0; l<current.length; l++) {
+            for(int c=0; c<current[l].length; c++) {
+                
+                int cell = current[l][c];
+
+                int left = (c > 0 ? current[l][c - 1] : 0) + (c > 1 ? current[l][c - 2] : 0);
+                int rigth = (c < current[l].length - 1 ? current[l][c + 1] : 0) + (c < current[l].length - 2 ? current[l][c + 2] : 0);
+                int top = (l > 0 ? current[l - 1][c] : 0) + (l > 1 ? current[l - 2][c] : 0);
+                int down = (l < current.length - 1 ? current[l + 1][c] : 0) + (l < current.length - 2 ? current[l + 2][c] : 0);
+
+                int simbiosis = (left+rigth+top+down);
+
+                if(simbiosis>=3){
+                    next[l][c]=1;
+                }
+                    else if(simbiosis<2){
+                        next[l][c]=0;
+                    }
+                    else if(simbiosis>5){
+                        next[l][c]=0;
+                    }
+                    else if(current[l][c]==0 && simbiosis==3){
+                        next[l][c]=1;
+                        }
+                    System.out.print(next[l][c]==1 ? " 1e:🦠 " : " 0e:☠️  ");    
+                    }
+                System.out.println();
+                System.out.println();
+            }
+        }
+    }
